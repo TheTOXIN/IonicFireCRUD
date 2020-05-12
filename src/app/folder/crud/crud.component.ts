f2a6588ba60ca2e0185b7b1ce4ff39ea2e9cafa9
TheTOXIN <thetoksin@gmail.com>
2020-05-12 20:51:51 +0300

    + fire crud

--- src/app/folder/crud/crud.component.ts
+++ src/app/folder/crud/crud.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FirebaseService} from '../../services/firebase.service';
+
+interface Student {
+  name: string;
+  age: number;
+  address: string;
+}
 
 @Component({
   selector: 'app-crud',
@@ -7,8 +15,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudComponent implements OnInit {
 
-  constructor() { }
+  studentList = [];
+  studentData: Student;
+  studentForm: FormGroup;
 
-  ngOnInit() {}
+  constructor(
+      private firebaseService: FirebaseService,
+      public fb: FormBuilder
+  ) {
+    this.studentData = {} as Student;
+  }
 
+  ngOnInit() {
+    this.studentForm = this.fb.group({
+      name: ['', [Validators.required]],
+      age: ['', [Validators.required]],
+      address: ['', [Validators.required]]
+    });
+
+    this.firebaseService.read().subscribe(data => {
+      this.studentList = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          name: e.payload.doc.data()['name'],
+          age: e.payload.doc.data()['age'],
+          address: e.payload.doc.data()['address'],
+        };
+      });
+      console.log(this.studentList);
+    });
+  }
+
+  create() {
+    console.log(this.studentForm.value);
+    this.firebaseService.create(this.studentForm.value).then(res => {
+      this.studentForm.reset();
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  public remove(rowId) {
+    this.firebaseService.delete(rowId);
+  }
+
+  public edit(record) {
+    record.isEdit = true;
+    record.editName = record.name;
+    record.editAge = record.age;
+    record.editAddress = record.address;
+  }
+
+  public update(recordRow) {
+    const record = {};
+    record['name'] = recordRow.editName;
+    record['age'] = recordRow.editAge;
+    record['address'] = recordRow.editAddress;
+    this.firebaseService.update(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
 }
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  collection = 'students';
+
+  constructor(
+      private firestore: AngularFirestore
+  ) {
+
+  }
+
+  create(record) {
+    return this.firestore
+        .collection(this.collection)
+        .add(record);
+  }
+
+  read() {
+    return this.firestore
+        .collection(this.collection)
+        .snapshotChanges();
+  }
+
+  update(recordId, record) {
+    this.firestore
+        .doc(this.collection + '/' + recordId)
+        .update(record);
+  }
+
+  delete(recordId) {
+    this.firestore
+        .doc(this.collection + '/' + recordId)
+        .delete();
+  }
+}
